09c9e6cc55320f73befeafac3beddd2effc48d8d
Etch <Etch.rush@gmail.com>
2018-06-28 23:48:51 -0800

    so close

--- choochoo.js
+++ choochoo.js
@@ -17,15 +17,26 @@ $("#submit").on("click", function () {
     var tName = $("#train-name").val();
     var destination = $("#destination").val();
     var firstTrain = $("#first-train").val();
-    var frequency = $("#frequency").val();
-    var currentTime = moment().
-    console.log(currentTime);
+    var frequency = $("#frequency").val();;
+    var firstTimeFormat = moment(firstTrain, "hh:mm").format("HH:mm A");
+    console.log(firstTimeFormat);
+    var timeDiff = moment().diff(moment(firstTimeFormat, "HH:mm"), "minutes");
+    var remainder = timeDiff % frequency;
+    var minutesTillTrain = frequency - remainder;
+    console.log(minutesTillTrain);
+    var nextTrain = moment().add(minutesTillTrain , "minutes").format("HH:mm A");
+    console.log(nextTrain);
+
+
 
     database.ref().push({
         TrainName: tName,
         Destination: destination,
         FirstTrain: firstTrain,
         Frequency: frequency,
+        NextTrain: nextTrain,
+        MinutesTill: minutesTillTrain,
+
     })
 
     $("input").val("");
@@ -33,7 +44,7 @@ $("#submit").on("click", function () {
 });
 
 database.ref().on("child_added", function (snapshot) {
-    var newTrain = "<tr>" + "<td>" + snapshot.val().TrainName + "<td>" + snapshot.val().Destination + "<td>" + snapshot.val().Frequency;
+    var newTrain = "<tr>" + "<td>" + snapshot.val().TrainName + "<td>" + snapshot.val().Destination + "<td>" + snapshot.val().Frequency + "<td>" + snapshot.val().NextTrain + "<td>" + snapshot.val().MinutesTill;
     $("tbody").append(newTrain);
 
 })
